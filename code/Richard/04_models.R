5ee84ef761b3a0b8f03f8bf1b9347db89e727afe
Emily Stringer <ejstringer@live.com.au>
2026-02-13 14:35:58 +1100

    spp added

--- code/Richard/04_models.R
+++ code/Richard/04_models.R
@@ -25,9 +25,10 @@ pest <- pest_detection %>%
          seed = ifelse(seed == 'no seed', 0, 1))
 
 ### glm model -----
-mFull <- glm(dna ~ grade + age + food + seed + wood + soil + simpsondna_nopest, data = pest,
+mFull <- glm(dna ~ grade + age + food + seed + wood + soil + simpsondna_nopest + no_countries, data = pest,
              family = binomial) 
 summary(mFull)
+anova(mFull)
 
 m <- glm(dna ~ grade + simpsondna_nopest, data = pest,
          family = binomial) 
@@ -37,16 +38,47 @@ summary(m)
 # Next step: Simpson as response ------------------------------------------
 
 ### glm model -----
-mFull_simpson <- glm(simpsondna_nopest ~ grade + age + food + seed + wood + soil + no_countries, 
+mFull_simpson <- lm(simpsondna_nopest ~ grade + age + food + seed + wood + soil, 
                      data = pest) 
 summary(mFull_simpson)
+anova(mFull_simpson)
 
-m <- glm(dna ~ grade + simpsondna_nopest, data = pest,
-         family = binomial) 
+m <- lm(simpsondna_nopest ~ grade + age, data = pest) 
 summary(m)
 
 
+# species specific separated by species model -----------------------------
 
+pest_spp <- pest_detection_spp %>% 
+  left_join(container) %>% 
+  mutate(dna = ifelse(pests_sppDNA > 0, 1, 0), 
+         grade = factor(container_grade, 
+                        levels = unique(container$container_grade)[c(2,3,1)])) %>% 
+  filter(complete.cases(age), complete.cases(container_size)) %>% 
+  mutate(food = ifelse(goods_risk == 'food', 1, 0),
+         seed = ifelse(seed == 'no seed', 0, 1))
+
+pest_spp %>% group_by(common_name) %>% summarise(dna = sum(dna))
+
+glm(dna ~ grade + age + food + seed + wood + soil + simpsondna_nopest, 
+             data = filter(pest_spp, common_name=="khapra beetle"),
+             family = binomial) %>% summary()
+
+glm(dna ~ grade + age + food + seed + wood + soil + simpsondna_nopest, 
+    data = filter(pest_spp, common_name=="asian spongy moth"),
+    family = binomial) %>% summary()
+
+glm(dna ~ grade + age + food + seed + wood + soil + simpsondna_nopest, 
+    data = filter(pest_spp, common_name=="brown marmorated stink bug"),
+    family = binomial) %>% summary()
+
+glm(dna ~ grade + age + food + seed + wood + soil + simpsondna_nopest, 
+    data = filter(pest_spp, common_name=="electric ant"),
+    family = binomial) %>% summary()
+
+glm(dna ~ grade + age + food + seed + wood + soil + simpsondna_nopest, 
+    data = filter(pest_spp, common_name=="spotted lantern fly"),
+    family = binomial) %>% summary()
 
 
 
