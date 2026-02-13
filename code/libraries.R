dda94cbe871163721649bb6204aef71b7b2d365b
Emily Stringer <ejstringer@live.com.au>
2026-02-13 14:44:09 +1100

    remove lib.loc

--- code/libraries.R
+++ code/libraries.R
@@ -1,19 +1,20 @@
-library(tidyverse, lib.loc = 'C:/Program Files/R/R-4.1.2/library/')
-library(lubridate, lib.loc = 'C:/Program Files/R/R-4.1.2/library/')
+library(tidyverse)
+library(lubridate)
 
 library(rmarkdown)
 
-library(grid, lib.loc = 'C:/Program Files/R/R-4.1.2/library/')
-library(gridExtra, lib.loc = 'C:/Program Files/R/R-4.1.2/library/')
+library(grid)
+library(gridExtra)
 
-library(png, lib.loc = 'C:/Program Files/R/R-4.1.2/library/')
+library(png)
 
 library(vegan)
 library(lme4)
 library(lmerTest)
 
-library(flextable, lib.loc = 'C:/Program Files/R/R-4.1.2/library/')
-library(officer, lib.loc = 'C:/Program Files/R/R-4.1.2/library/')
+library(flextable)
+library(officer)
 
 library(abdiv)
 #library(DescTools)
+
